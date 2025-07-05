375a57c4a3c61be51899e020f5db1685b5d5aa95
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-06 02:05:16 +0530

    mini.statusline modes are now in bold!

--- lua/frontier/highlights/plugins/mini/statusline.lua
+++ lua/frontier/highlights/plugins/mini/statusline.lua
@@ -5,16 +5,16 @@ local blue = require("frontier.theme.colors.blue")
 local green = require("frontier.theme.colors.green")
 local grey = require("frontier.theme.colors.grey")
 local red = require("frontier.theme.colors.red")
-
+local style = require("frontier.highlights.style")
 local M = {}
 
 M.CHANGES = {
-	MiniStatuslineModeNormal = { fg = grey.Lighter, bg = grey.Black },
-	MiniStatuslineModeInsert = { fg = grey.AlmostBlack, bg = green.Normal },
-	MiniStatuslineModeVisual = { fg = grey.AlmostBlack, bg = blue.Normal },
-	MiniStatuslineModeReplace = { fg = grey.Normal, bg = red.Normal },
-	MiniStatuslineModeCommand = { fg = grey.Loud, bg = red.Darker },
-	MiniStatuslineModeOther = { fg = grey.Black, bg = grey.Darker },
+	MiniStatuslineModeNormal = { fg = grey.Lighter, bg = grey.Black, style = style.Bold },
+	MiniStatuslineModeInsert = { fg = grey.AlmostBlack, bg = green.Normal, style = style.Bold },
+	MiniStatuslineModeVisual = { fg = grey.AlmostBlack, bg = blue.Normal, style = style.Bold },
+	MiniStatuslineModeReplace = { fg = grey.Normal, bg = red.Normal, style = style.Bold },
+	MiniStatuslineModeCommand = { fg = grey.Loud, bg = red.Darker, style = style.Bold },
+	MiniStatuslineModeOther = { fg = grey.Black, bg = grey.Darker, style = style.Bold },
 
 	-- Highlight used in default statusline
 	MiniStatuslineDevinfo = { fg = grey.AlmostBlack, bg = grey.Normal },

f49543192345a905d07616ee01467b307db28a43
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-05 18:40:54 +0530

    Diagnostic colors sorted!

--- lua/frontier/highlights/std/diagnostics.lua
+++ lua/frontier/highlights/std/diagnostics.lua
@@ -6,16 +6,20 @@ local cyan = require("frontier.theme.colors.cyan")
 local green = require("frontier.theme.colors.green")
 local grey = require("frontier.theme.colors.grey")
 local red = require("frontier.theme.colors.red")
+local yellow = require("frontier.theme.colors.yellow")
 
 local M = {}
 
 M.CHANGES = {
-	ErrorMsg = { fg = red.Brighter },
+	ErrorMsg = { fg = grey.Lighter, bg = red.Normal },
 	ModeMsg = { fg = green.Brighter, bg = grey.AlmostBlack },
-	MoreMsg = { fg = cyan.Normal },
-	WarningMsg = { fg = red.Normal },
-	DiagnosticInfo = { fg = green.Normal },
-	DiagnosticWarn = { fg = blue.Normal },
+	MoreMsg = { fg = grey.Black, bg = cyan.Normal },
+	WarningMsg = { fg = grey.Loud, bg = red.Normal },
+	DiagnosticError = { fg = grey.Black, bg = red.Brighter },
+	DiagnosticInfo = { fg = grey.Black, bg = cyan.Normal },
+	DiagnosticWarn = { fg = grey.Black, bg = yellow.Normal },
+	DiagnosticHint = { fg = grey.Black, bg = blue.Normal },
+	DiagnosticOk = { fg = grey.Black, bg = green.Normal },
 }
 
 M.LINKS = {}

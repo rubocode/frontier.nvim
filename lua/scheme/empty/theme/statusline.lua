36aed3f509faef1cdbfa26a129f1ae2789f86709
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-07 01:50:43 +0530

    Update statusline.lua

--- lua/scheme/empty/theme/statusline.lua
+++ lua/scheme/empty/theme/statusline.lua
@@ -6,10 +6,12 @@ local M = {
 	StatusLineTerm = {},
 	StatusLineTermNC = {},
 	StatusLine = {},
+	-- NEOVIM STATUSLINE SECTIONS
 	Devinfo = {},
 	Fileinfo = {},
 	Filename = {},
 	Inactive = {},
+	-- NEOVIM MODES
 	ModeCommand = {},
 	ModeInsert = {},
 	ModeNormal = {},

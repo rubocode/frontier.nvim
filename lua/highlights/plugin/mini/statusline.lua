d1b2260fea723fdedd78344cd8ce8b63ed588789
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-09 00:42:07 +0530

    let's centralize at the highlights!

--- lua/highlights/plugin/mini/statusline.lua
+++ lua/highlights/plugin/mini/statusline.lua
@@ -1,26 +1,24 @@
 -- MINI.STATUSLINE HIGHLIGHTS
 -- JUL 05, 2025
 
-local line = require("profile.plugin.mini.statusline")
+local display = require("theme.display")
+local mode = require("theme.mode")
 
 local M = {}
 
 M.URL = "https://github.com/echasnovski/mini.statusline"
 
 M.PROFILE = {
-	MiniStatuslineModeNormal = line.ModeNormal,
-	MiniStatuslineModeInsert = line.ModeInsert,
-	MiniStatuslineModeVisual = line.ModeVisual,
-	MiniStatuslineModeReplace = line.ModeReplace,
-	MiniStatuslineModeCommand = line.ModeCommand,
-	MiniStatuslineModeOther = line.ModeOther,
-	-- Highlight used in default statusline
-	MiniStatuslineDevinfo = line.Devinfo,
-	--   (|MiniStatusline.section_git| and |MiniStatusline.section_diagnostics|)
-	MiniStatuslineFilename = line.Filename,
-	MiniStatuslineFileinfo = line.Fileinfo,
-	-- Other groups,
-	MiniStatuslineInactive = line.Inactive,
+	MiniStatuslineDevinfo = display.Standard,
+	MiniStatuslineFileinfo = display.Standard,
+	MiniStatuslineFilename = display.Prominent,
+	MiniStatuslineInactive = display.Inactive,
+	MiniStatuslineModeCommand = mode.Line.Command,
+	MiniStatuslineModeInsert = mode.Line.Insert,
+	MiniStatuslineModeNormal = mode.Line.Normal,
+	MiniStatuslineModeOther = mode.Line.Other,
+	MiniStatuslineModeReplace = mode.Line.Replace,
+	MiniStatuslineModeVisual = mode.Line.Visual,
 }
 
 M.LINKS = {}

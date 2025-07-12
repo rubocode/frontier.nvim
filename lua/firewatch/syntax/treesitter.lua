4b539e738681747028ad570aa00569de094080f9
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-12 22:48:07 +0530

    Update treesitter.lua

--- lua/firewatch/syntax/treesitter.lua
+++ lua/firewatch/syntax/treesitter.lua
@@ -1,15 +1,14 @@
 -- SYNTAX BASIC THEME COLORS
 -- JUL 06, 2025
 
-local grey = require("frontier.palette.grey")
-local yellow = require("frontier.palette.yellow")
+local uno = require("firewatch.palette.uno")
 
 local M = {
-	FunctionBuiltin = { fg = yellow.Normal },
-	Module = { fg = yellow.Normal },
-	TypeBuiltin = { fg = yellow.Normal },
-	Variable = { fg = grey.Normal },
-	VariableParameter = { fg = grey.Normal },
+	FunctionBuiltin = { fg = uno.Uno3 },
+	Module = { fg = uno.Uno1 },
+	TypeBuiltin = { fg = uno.Uno3 },
+	Variable = { fg = uno.Uno2 },
+	VariableParameter = { fg = uno.Uno2 },
 }
 
 return M

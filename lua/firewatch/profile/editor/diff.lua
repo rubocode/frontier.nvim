6f379f3a10ca47ebd2ef28752534a2c4a3553650
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-16 13:29:57 +0530

    default.profile.editor.diff is now used by firewatch!

--- lua/firewatch/profile/editor/diff.lua
+++ lua/firewatch/profile/editor/diff.lua
@@ -1,16 +1,6 @@
 -- FIREWATCH EDITOR DIFF PROFILE
 -- JUL 15, 2025
 
-local diff = require("firewatch.map.diff")
-
-local M = {
-	DiffAdd = diff.Add,
-	DiffChange = diff.Change,
-	DiffDelete = diff.Delete,
-	DiffText = diff.Text,
-	Added = diff.Add,
-	Changed = diff.Change,
-	Removed = diff.Delete,
-}
+local M = require("default.profile.editor.diff")
 
 return M

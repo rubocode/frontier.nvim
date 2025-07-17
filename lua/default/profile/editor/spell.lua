9c087c398d08b530ce4a97e71f659d29ff18b0ed
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-17 23:26:01 +0530

    spelling moved out from general to separate file!

--- lua/default/profile/editor/spell.lua
+++ lua/default/profile/editor/spell.lua
@@ -1,20 +1,27 @@
 -- DEFAULT EDITOR SPELL PROFILE
 -- JUL 15, 2025
 
+local odd = require("empty.map.odd")
+local style = require("highlights.style")
+
+local mapper = require("util.mapper")
+
 local M = {}
 local profile = {}
 
--- TODO:  Not right now!
+-- TODO: Not right now!
 --
 
-local remap = function(_) end
+local remap = function(theme)
+	odd = mapper.load(theme, "map.odd")
+end
 
 local infuse = function()
 	profile = {
-		SpellBad = {},
-		SpellCap = {},
-		SpellLocal = {},
-		SpellRare = {},
+		SpellBad = { fg = odd.Normal, style = style.Undercurl },
+		SpellCap = { fg = odd.Lighter, style = style.Undercurl },
+		SpellLocal = { fg = odd.Normal, style = style.Undercurl },
+		SpellRare = { fg = odd.Normal, style = style.Undercurl },
 	}
 end
 

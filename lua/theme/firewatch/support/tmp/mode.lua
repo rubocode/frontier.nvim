3832aa5197450df8d3fd94943c1868021e06bfc4
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-18 17:40:21 +0530

    cleanup!

--- lua/theme/firewatch/support/tmp/mode.lua
+++
@@ -1,6 +0,0 @@
--- FIREWATCH DIFF MAP
--- JUL 13, 2025
-
-local mode = require("std.profile.mode")
-
-return mode
--- lua/theme/firewatch/support/tmp/view.lua
+++
@@ -1,6 +0,0 @@
--- FIREWATCH VIEW MAP
--- JUL 13, 2025
-
-local view = require("std.profile.view")
-
-return view

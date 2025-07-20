a5b09700d58d6a7b2fc349fc2b454a9e70ba9158
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-20 14:30:34 +0530

    style injection generalized to accept multiple styles in a highlight!

--- lua/util/style.lua
+++ lua/util/style.lua
@@ -1,11 +1,24 @@
 -- STYLE UTILITY
 -- JUL 19, 2025
 
+local notify = require("util.notify")
+
 local M = {}
 
 function M.inject(opts, style)
 	if style then
-		opts[style.text] = true
+		if style.text then
+			-- single value
+			opts[style.text] = true
+			notify.info("\t\tSTYLE: " .. style.text)
+		else
+			local text = nil
+			for _, v in pairs(style) do
+				text = table.concat({ text, v }, ",")
+			end
+			opts["style"] = text
+			notify.info("\t\tSTYLE: " .. text)
+		end
 	end
 	return opts
 end

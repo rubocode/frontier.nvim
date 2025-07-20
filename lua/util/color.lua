921d16143469d555704a000c1bd20339c3fdb46a
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-21 00:37:33 +0530

    Nvim (20) color palette is out of scope for the moment!

--- lua/util/color.lua
+++ lua/util/color.lua
@@ -8,8 +8,27 @@ local M = {}
 function M.inject(opts, color, xg)
 	if color then
 		notify.info("\t\tCOLOR: <" .. xg .. "> " .. M.display(color))
-		opts[xg] = color.hex
-		opts["cterm" .. xg] = color.index
+		if color.type == "xterm256" then
+			opts[xg] = color.hex
+			opts["cterm" .. xg] = color.index
+		elseif color.type == "neovim" then
+			-- It is not very clear to me at this time
+			-- how to define color options using the neovim color palette.
+			-- If you set "fg" in opts, nvim sets it up as "guifg".
+			-- This works under "truecolor" but acts strange
+			-- (in a way that I don't understand yet).
+			-- So, I will stick to my initial goal of designing xterm256
+			-- themes and give the neovim colors a rest.
+			-- Let it be out of scope.
+			-- I will leave the color palette in the project
+			-- but will report and error if a neovim color is used
+			-- anywhere.
+			-- This will merely indicate where to engage
+			-- if it becomes in scope in the future.
+
+			notify.error("Unexpected use of a NEOVIM color: " .. M.display(color))
+			-- opts[xg] = color.name
+		end
 	end
 	return opts
 end

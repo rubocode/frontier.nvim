576bf67dd652b9a91658e66c3a883b72f2b2e0f5
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-27 00:24:33 +0530

    Create canvas.lua

---
+++ lua/theme/apprentice/archetype/canvas.lua
@@ -0,0 +1,12 @@
+-- APPRENTICE CANVAS ARCHETYPE
+-- JUL 26, 2025
+
+local grey = require("theme.apprentice.palette.grey")
+
+local M = {
+	Darker = grey.AlmostBlack,
+	Normal = grey.Darker,
+	Lighter = grey.Dark,
+}
+
+return M

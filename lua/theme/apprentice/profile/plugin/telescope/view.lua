1a37ff300d7f4a9d2e8e14e25d6966b95ae27625
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-28 18:51:44 +0530

    Apprentice telescope selection fixed with style.Reverse!

--- lua/theme/apprentice/profile/plugin/telescope/view.lua
+++ lua/theme/apprentice/profile/plugin/telescope/view.lua
@@ -5,12 +5,14 @@ local canvas = require("theme.apprentice.archetype.canvas")
 local text = require("theme.apprentice.archetype.text")
 local uniq = require("theme.apprentice.archetype.unique")
 
+local style = require("highlights.style")
+
 local M = {
 	Border = { fg = uniq.Accent },
 	Prompt = { fg = uniq.Special },
-	Selection = { fg = canvas.Normal, bg = uniq.Accent },
+	Selection = { fg = uniq.Accent, bg = canvas.Normal, style = style.Reverse },
 	SelectionCaret = { fg = text.Normal, bg = canvas.Darker },
-	Special = { fg = uniq.Fixed },
+	Special = { fg = uniq.Special },
 	Title = { fg = uniq.Peculiar },
 }
 

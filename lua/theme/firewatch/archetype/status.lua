0e4a2b6d02dace4a11dc0a569f1832d77c690e8e
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-28 12:08:20 +0530

    Update status.lua

--- lua/theme/firewatch/archetype/status.lua
+++ lua/theme/firewatch/archetype/status.lua
@@ -5,8 +5,9 @@ local text = require("theme.firewatch.archetype.text")
 local uniq = require("theme.firewatch.archetype.unique")
 local canvas = require("theme.firewatch.archetype.canvas")
 
+local style = require("highlights.style")
 local M = {
-	Loud = { fg = canvas.Normal, bg = text.Quiet },
+	Loud = { fg = canvas.Normal, bg = text.Quiet, style = style.Bold },
 	Normal = { fg = canvas.Normal, bg = uniq.Dynamic },
 	Quiet = { fg = text.Quiet, bg = canvas.Normal },
 }

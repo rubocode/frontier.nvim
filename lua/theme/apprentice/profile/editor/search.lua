96f76f5e1da8b0db6fb32e243122133bae334841
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-30 18:34:08 +0530

    Apprentice Search adjusted to Telescope!

--- lua/theme/apprentice/profile/editor/search.lua
+++ lua/theme/apprentice/profile/editor/search.lua
@@ -1,13 +1,22 @@
 -- APPRENTICE EDITOR SEARCH PROFILE
 -- JUL 29, 2025
 
+local canvas = require("theme.apprentice.archetype.canvas")
+local uniq = require("theme.apprentice.archetype.unique")
+
 local grey = require("theme.apprentice.palette.grey")
-local light = require("theme.apprentice.palette.light")
 local std = require("theme.apprentice.palette.std")
+
+local style = require("highlights.style")
 
 local M = {
 	IncSearch = { fg = grey.Darker, bg = std.Red },
-	Search = { fg = grey.Darker, bg = light.Yellow },
+	-- JUL 30, 2025
+	-- Changed the original to display the selected text in
+	-- Telescope Highlights View.
+	--
+	-- Search = { fg = grey.Darker, bg = light.Yellow },
+	Search = { fg = uniq.Accent, bg = canvas.Normal, style = style.Reverse },
 }
 
 return M
